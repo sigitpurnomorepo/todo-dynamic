e300f1a227ee2c2a5b07fc884f76c1a391b27407
sigitzigit <sigit.officejkt@gmail.com>
2023-06-01 00:30:42 +0700

    Create script-login.js and write javascript connection code

---
+++ js/script-login.js
@@ -0,0 +1,19 @@
+const btnLogin = document.getElementById("btn-login");
+
+btnLogin.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email-address").value;
+    const password = document.getElementById("password").value;
+
+    if (email && password) {
+        if (email == "sigitpurnomo@gmail.com" && password == "12345") {
+            // Redirect to todo page
+            window.location.href = "todo.html";
+        } else {
+            window.alert("Email atau Password salah!")
+        }
+    } else {
+        window.alert("Email dan Password tidak boleh kosong!");
+    }
+})
