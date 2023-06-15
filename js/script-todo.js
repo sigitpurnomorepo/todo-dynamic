00a0082915a8b996b11176e1d95290fc9126c9ec
sigit purnomo <sigitoffice.jkt@gmail.com>
2023-06-15 09:27:20 +0700

    Update file todo.html & script-todo.js from todo-hardCode

--- js/script-todo.js
+++ js/script-todo.js
@@ -1,20 +1,21 @@
-function dragStart(event) {
-    event.dataTransfer.setData("judul-item", event.target.id);
-}
-
+// Drag n Drop
 function allowDrop(event) {
     event.preventDefault();
 }
 
+function dragStart(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
 function drop(event) {
     event.preventDefault();
-    const data = event.dataTransfer.getData("judul-item");
+    const data = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(data));
 }
 
-// CRUD
-const todo = document.getElementById("todo-item");
+// Fungsi Add Task
 const btnAdd = document.getElementById("btn-add");
+const todo = document.getElementById("todo-item");
 btnAdd.addEventListener("click", (event) => {
     event.preventDefault();
 
@@ -24,69 +25,78 @@ btnAdd.addEventListener("click", (event) => {
     // Tes pengambilan value
     //console.log(title, description);
 
-    const card = document.createElement("div");
-    const article = document.createElement("article");
-    const h3 = document.createElement("h3");
-    const p = document.createElement("p");
-    const btnWrapper = document.createElement("div");
-    const btnEdit = document.createElement("button");
-    const btnDelete = document.createElement("button");
+    // Validasi dalam proses create task baru
+    if (title && description != "") {
+        const card = document.createElement("div");
+        const article = document.createElement("article");
+        const h3 = document.createElement("h3");
+        const p = document.createElement("p");
+        const btnWrapper = document.createElement("div");
+        const btnEdit = document.createElement("button");
+        const btnDelete = document.createElement("button");
 
-    card.setAttribute("class", "card p-4 my-1");
-    card.setAttribute("ondragstart", "dragstart(event)");
-    card.setAttribute("draggable", "true");
-    card.setAttribute("id", title);
+        card.setAttribute("class", "card p-4 my-1");
+        card.setAttribute("ondragstart", "dragStart(event)");
+        card.setAttribute("draggable", "true");
+        card.setAttribute("id", title);
 
-    h3.appendChild(document.createTextNode(title));
-    p.appendChild(document.createTextNode(description));
+        h3.appendChild(document.createTextNode(title));
+        p.appendChild(document.createTextNode(description));
 
-    article.appendChild(h3);
-    article.appendChild(p);
+        article.appendChild(h3);
+        article.appendChild(p);
 
-    btnWrapper.setAttribute("class", "d-flex flex-row gap-1");
-    btnEdit.setAttribute("type", "button");
-    btnEdit.setAttribute("class", "btn-edit rounded-2 btn btn-info text-white");
-    btnEdit.appendChild(document.createTextNode("Edit"));
-    btnDelete.setAttribute("type", "button");
-    btnDelete.setAttribute("class", "btn-delete rounded-2 btn btn-danger");
-    btnDelete.appendChild(document.createTextNode("Delete"));
+        btnWrapper.setAttribute("class", "d-flex flex-row gap-1");
+        btnEdit.setAttribute("type", "button");
+        btnEdit.setAttribute("class", "btn btn-info text-white");
+        btnEdit.setAttribute("onclick", "handleEdit(this.id)");
+        btnEdit.setAttribute("id", "edit-" + description);
+        btnEdit.appendChild(document.createTextNode("Edit"));
+        btnDelete.setAttribute("type", "button");
+        btnDelete.setAttribute("class", "btn btn-danger");
+        btnDelete.setAttribute("onclick", "handleDelete(this.id)");
+        btnDelete.setAttribute("id", "delete-" + description);
+        btnDelete.appendChild(document.createTextNode("Delete"));
 
-    btnWrapper.appendChild(btnEdit);
-    btnWrapper.appendChild(btnDelete);
+        btnWrapper.appendChild(btnEdit);
+        btnWrapper.appendChild(btnDelete);
 
-    card.appendChild(article);
-    card.appendChild(btnWrapper);
+        article.appendChild(btnWrapper);
+        card.appendChild(article);
 
-    // todo.setAttribute("ondrop", "drop(event)");
-    // todo.setAttribute("ondragover", "allowDrop(event)");
-    // todo.setAttribute("id", "todo-item");
-    // todo.setAttribute("ondrop", "drop(event)");
-    // todo.setAttribute("ondragover", "allowDrop(event)");
-    // todo.setAttribute("id", "done-item");
-
-    todo.appendChild(card);
+        todo.appendChild(card);
+    } else {
+        alert("Inputan tidak boleh kosong!")
+    }
 })
 
+// Fungsi edit task
 function handleEdit(id) {
-    const title = window.prompt("Masukkan Judul");
-    const description = window.prompt("Masukkan Description");
+    const title = window.prompt("Ganti Judul");
+    const description = window.prompt("Ganti Description");
 
-    const btnEdit = document.getElementById(id);
-    const article = btnEdit.parentNode.parentNode;
-    const h3 = article.firstElementChild;
-    const p = article.firstElementChild.nextSibling.nextSibling;
+    if (title && description != "") {
+        const btnEdit = document.getElementById(id);
+        const article = btnEdit.parentNode.parentNode;
+        const h3 = article.firstElementChild;
+        const p = article.firstElementChild.nextElementSibling;
 
-    h3.innerHTML = title;
-    p.innerHTML = description;
+        h3.innerHTML = title;
+        p.innerHTML = description;
+    } else {
+        alert("Inputan tidak boleh kosong!")
+    }
 }
 
+// Fungsi delete task
 function handleDelete(id) {
     const btnDelete = document.getElementById(id);
-    const card = btnDelete.parentNode.parentNode;
+    const card = btnDelete.parentNode.parentNode.parentNode;
 
     card.setAttribute("class", "d-none");
 }
 
+// Fungsi jam realtime
 function showRealtimeClock() {
     const footerRealtimeClock = document.getElementById("footer-time");
     const time = new Date();
